c29a45abe490666bf616b8c57698aa1aa601d91e
mrjimin <z8888788@163.com>
2023-04-01 08:44:29 +0800

    chore(config): git提交规范配置
    增加scopes
    测试一下
    测试一下提交时配置是否生效

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -31,12 +31,31 @@ module.exports = {
         { value: 'revert', name: 'revert:   回退代码 | Revert to a commit' },
         { value: 'chore', name: 'chore:    其他修改 | Other changes that do not modify src or test files' },
       ],
+
       useEmoji: false,
       emojiAlign: 'center',
       useAI: false,
       aiNumber: 1,
       themeColorCode: '',
-      scopes: [],
+    //   scopes: [],
+        // scope 类型（定义之后，可通过上下键选择）
+  scopes: [
+    ['components', '组件相关'],
+    ['hooks', 'hook 相关'],
+    ['utils', 'utils 相关'],
+    ['element-ui', '对 element-ui 的调整'],
+    ['styles', '样式相关'],
+    ['deps', '项目依赖'],
+    ['config', '配置相关'],
+    ['other', '其他修改'],
+    // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
+    ['custom', '以上都不是？我要自定义']
+  ].map(([value, description]) => {
+    return {
+      value,
+      name: `${value.padEnd(30)} (${description})`
+    }
+  }),
       allowCustomScopes: true,
       allowEmptyScopes: true,
       customScopesAlign: 'bottom',
