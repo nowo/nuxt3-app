81677fb44556a6fab7e049c602f5795d8876538e
mrjimin <z8888788@163.com>
2023-04-11 23:17:01 +0800

    feat(other): webStock测试
    安装ws模块，使用webStock技术前后端建立通信

---
+++ server/api/socket.ts
@@ -0,0 +1,17 @@
+import { WebSocketServer } from 'ws'
+
+const wss = new WebSocketServer({ port: 8080 })
+
+wss.on('connection', (ws) => {
+  ws.on('error', console.error)
+
+  ws.on('message', (data) => {
+    console.log('received: %s', data)
+  })
+
+  ws.send('something')
+})
+
+export default defineEventHandler(async () => {
+  return ''
+})
